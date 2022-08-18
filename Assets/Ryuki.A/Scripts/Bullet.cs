87d92086aa8db19ed85aab0b2ff7260a4540a2ee
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-08-18 14:41:34 +0900

    新井 スクリプト改修

--- Assets/Ryuki.A/Scripts/Bullet.cs
+++ Assets/Ryuki.A/Scripts/Bullet.cs
@@ -8,14 +8,12 @@ public class Bullet : MonoBehaviour
     [SerializeField, Tooltip("î≠éÀë¨ìx")] int _power;
     [SerializeField, Tooltip("è¡ñ≈éûä‘")] float deleteTime;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
         _rb2d.AddForce(Vector2.right * _power, ForceMode2D.Impulse);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Destroy(gameObject, deleteTime);
