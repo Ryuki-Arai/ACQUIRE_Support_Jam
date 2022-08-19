d6db12c2556462917ca25450a5f06e5c5309610f
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-08-19 15:31:15 +0900

    敵のオブジェクトをプレハブ化

---
+++ Assets/Okada/MyScripts/EnemyAttack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField, Header("モーションアニメ")] Animator[] _animators;
+
+    [SerializeField] GameObject _zangekiObject;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    
+}
