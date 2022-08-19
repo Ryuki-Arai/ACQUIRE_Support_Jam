f62ccc9f8e3957973ccb2685683da672740811d3
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-08-19 16:41:26 +0900

    スクリプト改修

--- Assets/Okada/MyScripts/ItemSingleton.cs
+++ Assets/Okada/MyScripts/ItemSingleton.cs
@@ -7,11 +7,23 @@ public class ItemSingleton : MonoBehaviour
     //public static ItemSingleton Instance;
 
     static private ItemSingleton _instance = new ItemSingleton();
-    static public ItemSingleton Instance => _instance;
+    static public ItemSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var obj = new GameObject(typeof(ItemSingleton).Name);
+                _instance = obj.AddComponent<ItemSingleton>();
+            }
+            return _instance;
+        }
+    }
     private ItemSingleton() { }
 
     void Awake()
     {
+        _instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
