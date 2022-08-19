fc4b83ffb1c0b0630b694080460c800470c0a078
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-08-19 13:57:09 +0900

    アイテムのUIを見やすくした

--- Assets/Ryuki.A/Scripts/PlayerAttack.cs
+++ Assets/Ryuki.A/Scripts/PlayerAttack.cs
@@ -18,13 +18,20 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField, Tooltip("斬撃エフェクト")] GameObject _effect;
     SpriteRenderer _sr;
     [SerializeField, Tooltip("プロパティ使用")] ItemCatch _ic;
+    Animator _anim;
     Mode _mode;
+
+    public Mode Mode
+    {
+        get => _mode;
+    }
 
     void Start()
     {
         _sr = GetComponent<SpriteRenderer>();
         _ic.GetComponent<ItemCatch>();
         _mode = Mode.shuriken;
+        _anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -32,20 +39,35 @@ public class PlayerAttack : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("斬");
+            
         }
         else if (Input.GetMouseButtonDown(1) && _ic.Kunai > 0 && _mode == Mode.kunai)
         {
             Instantiate(_kunai, _mazzle.position,Quaternion.identity);
             _ic.Kunai--;
+            
         }
         else if(Input.GetMouseButtonDown(1) && _ic.Shuriken > 0 && _mode == Mode.shuriken)
         {
             Instantiate(_shuriken, _mazzle.position, Quaternion.identity);
             _ic.Shuriken--;
+            
         }
         if (Input.GetMouseButtonDown(2))
         {
             _mode = _mode == Mode.shuriken ? Mode.kunai : Mode.shuriken;
+            _ic.Shuriken += 0;
+            _ic.Kunai += 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_anim)
+        {
+            _anim.SetBool("Shuriken", Input.GetMouseButtonDown(0));
+            _anim.SetBool("Kunai", Input.GetMouseButtonDown(1) && _mode == Mode.kunai);
+            _anim.SetBool("Slash", Input.GetMouseButtonDown(1) && _mode == Mode.shuriken);
         }
     }
 }
