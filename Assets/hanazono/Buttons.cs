987471dbd0cff0db48e9f1c09b3cc1bd8d4381e6
hanaryou0730 <hanaryo730@gmail.com>
2022-08-19 16:52:31 +0900

    完成

--- Assets/hanazono/Buttons.cs
+++ Assets/hanazono/Buttons.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
     public void TitleButton()
     {
         FadeManager.Instance.LoadScene("TitleScene", 1.0f);
+    }
+    public void TitleButton1()
+    {
+       SceneManager.LoadScene("TitleScene");
     }
 
     public void ExitButton()
@@ -17,7 +22,10 @@ public class Buttons : MonoBehaviour
     Application.Quit();
 #endif
     }
-
+    public void StaffButton()
+    {
+        FadeManager.Instance.LoadScene("StaffRoll", 3.0f);
+    }
     public void arasujiButton()
     {
         FadeManager.Instance.LoadScene("arasuji", 3.0f);
--- Assets/hanazono/arasuji.cs
+++ Assets/hanazono/arasuji.cs
@@ -9,8 +9,8 @@ public class arasuji : MonoBehaviour
     [SerializeField] Text text;
     [SerializeField] float novelSpeed;//一文字一文字の表示する速さ
     int novelListIndex = 0; //現在表示中の会話文の配列
-    bool t = false;//フラグ
-    float time = 0;
+    
+    
     void Times1()
     {
         StartCoroutine(Novel());
@@ -35,7 +35,7 @@ public class arasuji : MonoBehaviour
  
     private IEnumerator Novel()
     {
-        time += Time.deltaTime;
+     
         
             int messageCount = 0; //現在表示中の文字数
             /* text.text = ""; //テキストのリセット*/
